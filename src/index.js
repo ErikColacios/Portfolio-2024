a5e3270b549c07213fbd8ea010261300ba2a95d8
ErikColacios <ecolaciosgarcia@gmail.com>
2024-01-07 23:18:48 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,71 @@
+// Navbar burger
+let burger = document.getElementById("burger");
+
+// Hidden navbar (appears in smaller viewports)
+let navItemsHidden = document.getElementById("navItemsHidden");
+
+// Project divs
+let divVideo1 = document.getElementById("divVideo1");
+let divVideo2 = document.getElementById("divVideo2");
+let divVideo3 = document.getElementById("divVideo3");
+let divVideo4 = document.getElementById("divVideo4");
+
+// Video screen
+let video = document.getElementById("videoPantalla");
+
+
+burger.addEventListener("click",()=>{
+    if(navItemsHidden.classList.contains("hidden")){
+        navItemsHidden.classList.remove("hidden")
+    }else{
+        navItemsHidden.classList.add("hidden")
+    }
+})
+
+divVideo1.addEventListener("click",()=>{
+    divVideo2.classList.remove("outline");
+    divVideo2.classList.replace("bg-gray-600", "bg-gray-800");
+    divVideo3.classList.remove("outline");
+    divVideo3.classList.replace("bg-gray-600", "bg-gray-800");
+    divVideo4.classList.remove("outline");
+    divVideo4.classList.replace("bg-gray-600", "bg-gray-800");
+    video.src = "/src/videos/fustadecor_video.mp4";
+    divVideo1.classList.add("outline");
+    divVideo1.classList.replace("bg-gray-800", "bg-gray-600");
+})
+
+divVideo2.addEventListener("click",()=>{
+    divVideo1.classList.remove("outline");
+    divVideo1.classList.replace("bg-gray-600", "bg-gray-800");
+    divVideo3.classList.remove("outline");
+    divVideo3.classList.replace("bg-gray-600", "bg-gray-800");
+    divVideo4.classList.remove("outline");
+    divVideo4.classList.replace("bg-gray-600", "bg-gray-800");
+    video.src = "/src/videos/onepieceapp_video.mp4";
+    divVideo2.classList.add("outline");
+    divVideo2.classList.replace("bg-gray-800", "bg-gray-600");
+})
+
+divVideo3.addEventListener("click",()=>{
+    divVideo1.classList.remove("outline");
+    divVideo1.classList.replace("bg-gray-600", "bg-gray-800");
+    divVideo2.classList.remove("outline");
+    divVideo2.classList.replace("bg-gray-600", "bg-gray-800");
+    divVideo4.classList.remove("outline");
+    divVideo4.classList.replace("bg-gray-600", "bg-gray-800");
+    video.src = "/src/videos/randompass_video.mp4";
+    divVideo3.classList.add("outline");
+    divVideo3.classList.replace("bg-gray-800", "bg-gray-600");
+})
+
+divVideo4.addEventListener("click",()=>{
+    divVideo1.classList.remove("outline");
+    divVideo1.classList.replace("bg-gray-600", "bg-gray-800");
+    divVideo2.classList.remove("outline");
+    divVideo2.classList.replace("bg-gray-600", "bg-gray-800");
+    divVideo3.classList.remove("outline");
+    divVideo3.classList.replace("bg-gray-600", "bg-gray-800");
+    video.src = "/src/videos/greendrop_video.mp4";
+    divVideo4.classList.add("outline");
+    divVideo4.classList.replace("bg-gray-800", "bg-gray-600");
+})
