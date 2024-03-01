82d1c68e6f47c0d9a1b32cbbe49ce781e8c30a48
ErikColacios <ecolaciosgarcia@gmail.com>
2024-03-01 15:38:05 +0100

    commit send email

--- src/index.js
+++ src/index.js
@@ -53,3 +53,41 @@ divVideo4.addEventListener("click",()=>{
     video.src = "/src/videos/greendrop_video.mp4";
     divVideo4.classList.add("outline");
 })
+
+// Send mail via contact form
+const form = document.querySelector("form");
+const contact_name = document.getElementById("contact_name")
+const contact_company = document.getElementById("contact_company")
+const contact_phone = document.getElementById("contact_phone")
+const contact_email = document.getElementById("contact_email")
+const contact_message = document.getElementById("contact_message")
+
+/**
+ * Sends the submited values of the contact form to my personal email.
+ * I used SMTPJS scripts to connect to EmailAPI, where I configured my SMTP credentials. 
+ * https://smtpjs.com/
+ * https://app.elasticemail.com/
+ */
+function sendEmail() {
+    const bodyMessage = `Name: ${contact_name.value} <br>
+                        Company: ${contact_company.value} <br>
+                        Phone: ${contact_phone.value} <br>
+                        Email: ${contact_company.value} <br>
+                        Message: ${contact_message.value}`;
+
+    Email.send({
+        SecureToken: "e6b27b97-aa41-4b61-8d75-9968e5d18cb2",
+        To : 'ecolaciosgarcia@gmail.com',
+        From : "contact-portfolio@erikcolacios.com",
+        Subject : "Mensaje desde mi portfolio",
+        Body : bodyMessage
+    }).then(
+      message => alert(message)
+    );
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    sendEmail();
+})
